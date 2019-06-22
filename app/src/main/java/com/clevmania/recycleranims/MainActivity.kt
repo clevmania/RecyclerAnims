c492142ea8fec625b507456d529060edee8011e0
clevmania <ernestcleverest@gmail.com>
2019-06-22 08:24:45 +0100

    modified gradle

--- app/src/main/java/com/clevmania/recycleranims/MainActivity.kt
+++ app/src/main/java/com/clevmania/recycleranims/MainActivity.kt
@@ -2,6 +2,8 @@ package com.clevmania.recycleranims
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,8 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_main)
         populateView()
+//        supportActionBar?.hide()
     }
 
     private fun populateView(){
